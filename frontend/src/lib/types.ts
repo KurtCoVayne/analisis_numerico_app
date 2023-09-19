7dd51627b3f9037de25b3fb2563503ab3131ffe8
John Alejandro Gonzalez <john.gonzalez@ideaslab.com.co>
2023-09-19 01:06:58 -0500

    Created first frontend connection for symbolic root finder

--- frontend/src/lib/types.ts
+++ frontend/src/lib/types.ts
@@ -1,20 +1,36 @@
-import { z } from "zod";
+import { z } from "zod"
 
-const SymbolicRootsParams = z.object({ expression: z.string() }).passthrough();
-const ValidationError = z
+export const SymbolicRootsParams = z.object({ expression: z.string() })
+export const SymbolicRoots = z.object({ expression: z.string(), roots: z.string() })
+export const ValidationError = z
     .object({ loc: z.array(z.union([z.string(), z.number()])), msg: z.string(), type: z.string() })
-    .passthrough();
-const HTTPValidationError = z
+    
+export const HTTPValidationError = z
     .object({ detail: z.array(ValidationError) })
     .partial()
-    .passthrough();
-const BisectionRootsParams = z
+    
+export const BisectionRootsParams = z
     .object({ expression: z.string(), a: z.number(), b: z.number(), tol: z.number(), niter: z.number().int() })
-    .passthrough();
-
-export const schemas = {
-    SymbolicRootsParams,
-    ValidationError,
-    HTTPValidationError,
-    BisectionRootsParams,
-};
+    
+export const BisectionIteration = z
+    .object({
+        iteration: z.number().int(),
+        a: z.number(),
+        b: z.number(),
+        xm: z.number(),
+        f_xm: z.number(),
+        error: z.number(),
+    })
+    
+export const BisectionRoots = z
+    .object({ expression: z.string(), root: z.number(), table: z.array(BisectionIteration) })
+    
+export const NewtonRootsParams = z
+    .object({ expression: z.string(), x0: z.number(), tol: z.number(), niter: z.number().int() })
+    
+export const NewtonIteration = z
+    .object({ iteration: z.number().int(), x: z.number(), f_x: z.number(), f_prime_x: z.number(), error: z.number() })
+    
+export const NewtonRoots = z
+    .object({ expression: z.string(), derivative: z.string(), root: z.number(), table: z.array(NewtonIteration) })
+    
